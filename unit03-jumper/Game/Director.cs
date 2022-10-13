d1c56077d01c15bb5d8860fe4d735272d1cadae5
kyfrostbyte <94023660+kyfrostbyte@users.noreply.github.com>
2022-10-12 22:14:11 -0600

    Word class generates a sinlge word

--- unit03-jumper/Game/Director.cs
+++ unit03-jumper/Game/Director.cs
@@ -5,33 +5,15 @@ namespace Jumper.Game
     {
         public TerminalService _terminalService = new TerminalService();
         private Jumper _jumper = new Jumper();
-        public Words _words = new Words();
-        public List<string> _wordList = new List<string>(15);
+        public Words _goal = new Words();
         string userGuess = "";
         public Director()
         {
-            _wordList.Add("monkey");
-            _wordList.Add("orange");
-            _wordList.Add("watermelon");
-            _wordList.Add("computer");
-            _wordList.Add("charger");
-            _wordList.Add("parachute");
-            _wordList.Add("terminal");
-            _wordList.Add("gatorade");
-            _wordList.Add("library");
-            _wordList.Add("sweatshirt");
-            _wordList.Add("skateboard");
-            _wordList.Add("silver");
-            _wordList.Add("basketball");
-            _wordList.Add("wildcat");
-            _wordList.Add("chicken");
+            
         }
-
         public void RunGame()
         {
-            foreach(string a in _wordList)
-            _terminalService.WriteText(a);
-            // GetInput();
+            GetInput();
             
         }
 
--- unit03-jumper/Game/Words.cs
+++ unit03-jumper/Game/Words.cs
@@ -4,14 +4,32 @@ using System.Collections.Generic;
 namespace Jumper.Game
 {
     
-public class Words
-{
-    
-    public Words()
+    public class Words
     {
-        
+        public List<string> _wordOptions = new List<string>();  
+        public string _finalWord = "";
+        public Words()
+        {
+        _wordOptions.Add("monkey");
+        _wordOptions.Add("orange");
+        _wordOptions.Add("watermelon");
+        _wordOptions.Add("computer");
+        _wordOptions.Add("charger");
+        _wordOptions.Add("parachute");
+        _wordOptions.Add("terminal");
+        _wordOptions.Add("gatorade");
+        _wordOptions.Add("library");
+        _wordOptions.Add("sweatshirt");
+        _wordOptions.Add("skateboard");
+        _wordOptions.Add("silver");
+        _wordOptions.Add("basketball");
+        _wordOptions.Add("wildcat");
+        _wordOptions.Add("chicken"); 
+
+        Random random = new Random();
+        _finalWord = _wordOptions[random.Next(1, 16)];
+        }
     }
-}
 }
 
 
